63c72f17cec15088279d6c106cc965694bf83ece
Majo Vlasanovich <majo@vlasanovich.com>
2021-01-20 08:27:28 -0300

    Refactor endpoint to separate controller.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
-const saltRounds = 10;
 const cors = require('cors');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const imageSubmit = require('./controllers/image');
 
 const knex = require('knex')({
   client: 'pg',
@@ -13,6 +15,7 @@ const knex = require('knex')({
   },
 });
 
+const saltRounds = 10; // for bcrypt
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
@@ -27,90 +30,16 @@ app.get('/', function (req, res) {
   res.send('This is working');
 });
 
-app.post('/signin', (req, res) => {
-  knex
-    .select('email', 'hash')
-    .from('login')
-    .where('email', '=', req.body.email)
-    .then((data) => {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-      if (isValid) {
-        return knex
-          .select('*')
-          .from('users')
-          .where('email', '=', req.body.email)
-          .then((user) => {
-            res.json(user[0]);
-          })
-          .catch((err) => res.status(400).json('unable to get user'));
-      } else {
-        res.status(400).json('wrong credentials');
-      }
-    })
-    .catch((err) => res.status(400).json('wrong credentials'));
-});
+app.post('/signin', (req, res) => signin.handleSignin(req, res, knex, bcrypt));
 
-app.post('/register', (req, res) => {
-  const { email, name, password } = req.body;
-  const hash = bcrypt.hashSync(password, saltRounds);
-  // We have to update the users and login tables
-  knex
-    .transaction((trx) => {
-      trx
-        .insert({
-          hash: hash,
-          email: email,
-        })
-        .into('login')
-        .returning('email')
-        .then((loginEmail) => {
-          return trx('users')
-            .returning('*')
-            .insert({
-              email: loginEmail[0],
-              name: name,
-              joined: new Date(),
-            })
-            .then((user) => {
-              res.json(user[0]);
-            });
-        })
-        .then(trx.commit)
-        .catch(trx.rollback);
-    })
-    .catch((err) => res.status(400).json('unable to register'));
-});
+app.post('/register', (req, res) =>
+  register.handleRegister(req, res, knex, bcrypt, saltRounds)
+);
 
-app.get('/profile/:id', (req, res) => {
-  const { id } = req.params;
-  knex
-    .select('*')
-    .from('users')
-    .where({ id })
-    .then((user) => {
-      if (user.length) {
-        res.json(user[0]);
-      } else {
-        res.status(400).json('Not found');
-      }
-    })
-    .catch((err) => res.status(400).json('error getting user'));
-});
+app.get('/profile/:id', (req, res) => res.send('Work in progress.'));
 
-app.put('/image', (req, res) => {
-  const { id } = req.body;
-  knex('users')
-    .where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then((entries) => {
-      res.json(entries[0]);
-    })
-    .catch((err) => res.status(400).json('unable to get entries'));
-});
+app.put('/image', (req, res) => imageSubmit.handleImageSubmit(req, res, knex));
 
-app.get('*', (req, res) => {
-  res.json('not found');
-});
+app.get('*', (req, res) => res.json('not found'));
 
 app.listen(3000);
