9da87b5fe8ddb7b15437b90dd0617d4d8ea4e1f4
Majo Vlasanovich <majo@vlasanovich.com>
2021-01-22 06:37:56 -0300

    Add validation for inputs on submit and signin validation

--- controllers/register.js
+++ controllers/register.js
@@ -1,5 +1,8 @@
 function handleRegister(req, res, knex, bcrypt, saltRounds) {
   const { email, name, password } = req.body;
+  if (!email || !name || !password) {
+    return res.status(400).json('incorrect form submission');
+  }
   const hash = bcrypt.hashSync(password, saltRounds);
   // We have to update the users and login tables
   knex
